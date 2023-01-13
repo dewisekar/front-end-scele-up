9026ae57a40c77b74c11d86159b98d3392f13f15
Dewi Sekarini <dewi.sekarini@btpn.com>
2023-01-13 17:38:40 +0700

    update post view

--- src/views/maintainkol/UpdatePost.js
+++ src/views/maintainkol/UpdatePost.js
@@ -1,18 +1,16 @@
 import React, { useState, useEffect, Suspense } from 'react'
 import { useSearchParams } from 'react-router-dom'
-import { CCard, CCardBody, CCardHeader, CCol, CRow, CFormInput } from '@coreui/react'
-import Select from 'react-select'
+import { CCard, CCardBody, CCardHeader, CCol, CRow, CFormInput, CButton } from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { cilCheckCircle, cilXCircle } from '@coreui/icons'
 
 import { tableField } from './UpdatePost.config'
 import { getRequestByUri } from '../../utils/request-marketing'
 import { URL, PostStatus, DateMode } from 'src/constants'
-import { VerticalTableRow } from 'src/components'
 import { getPostStatus, convertDate } from 'src/utils/pageUtil'
-import { convertDataToSelectOptions } from '../../utils/GeneralFormInput'
 
 const UpdatePost = () => {
   const [searchParams] = useSearchParams()
-  const [postDetail, setPostDetail] = useState(null)
   const [state, setState] = useState({})
 
   useEffect(() => {
@@ -22,18 +20,20 @@ const UpdatePost = () => {
       const fetchData = async () => {
         const { message: fetchedDetail } = await getRequestByUri(URL.GET_POST_DETAIL + postId)
 
-        const { uploadDate, deadlineDate } = fetchedDetail
+        const { uploadDate, deadlineDate, postLink } = fetchedDetail
         const convertedUploadDate = uploadDate ? new Date(uploadDate) : null
 
         const postStatus = PostStatus[getPostStatus(new Date(deadlineDate), convertedUploadDate)]
         const convertedDeadline = convertDate(deadlineDate, DateMode.YYYYMMDD)
         const convertedUpload = uploadDate ? convertDate(deadlineDate, DateMode.YYYYMMDD) : null
+        const isLinkChecked = postLink ? true : false
 
-        setPostDetail({
+        setState({
           ...fetchedDetail,
           postStatus,
           deadlineDate: convertedDeadline,
           uploadDate: convertedUpload,
+          isLinkChecked,
         })
       }
 
@@ -46,7 +46,6 @@ const UpdatePost = () => {
   const onFormChange = (e) => {
     const { name, value, checked, type } = e.target
     const newVal = type === 'checkbox' ? checked : value
-    console.log('ini newVal', newVal)
 
     setState({
       ...state,
@@ -70,16 +69,75 @@ const UpdatePost = () => {
         <CCol xs={3}>
           <div className="p-2 border bg-light">{label}</div>
         </CCol>
-        <CCol xs={9}>
-          <CFormInput
-            type={type}
-            disabled={isFormDisabled}
-            name={field}
-            value={data[field]}
-            onChange={onFormChange}
-          />
-        </CCol>
+        {field === 'postLink' ? (
+          <>
+            <CCol xs={6}>
+              <CFormInput
+                type={type}
+                disabled={isFormDisabled}
+                name={field}
+                value={data[field]}
+                onChange={onFormChange}
+              />
+            </CCol>
+            <CCol xs={3} className="d-flex align-items-start justify-content-end">
+              <CButton
+                color="light"
+                className="w-100"
+                // onClick={}
+              >
+                Check Link Validity
+              </CButton>
+            </CCol>
+          </>
+        ) : (
+          <CCol xs={9}>
+            <CFormInput
+              type={type}
+              disabled={isFormDisabled}
+              name={field}
+              value={data[field]}
+              onChange={onFormChange}
+            />
+          </CCol>
+        )}
       </CRow>
+    )
+  }
+
+  const renderFormButtons = () => {
+    return (
+      <>
+        <CRow className="mb-1">
+          <CCol xs={3}>
+            <div className="p-2 border bg-light">Link Belum / Telah Dicek</div>
+          </CCol>
+          {state.isLinkChecked ? (
+            <CCol xs={3}>
+              <div className="p-2 border bg-light text-success text-center">
+                <b>Link Valid</b> <CIcon icon={cilCheckCircle} size="sm" />
+              </div>
+            </CCol>
+          ) : (
+            <CCol xs={3}>
+              <div className="p-2 border bg-light text-danger text-center">
+                <b>Link Tidak Valid</b> <CIcon icon={cilXCircle} size="sm" />
+              </div>
+            </CCol>
+          )}
+        </CRow>
+        <CRow className="mt-3">
+          <CCol xs={12}>
+            <CButton
+              color="info"
+              className="w-100"
+              // onClick={}
+            >
+              Update
+            </CButton>
+          </CCol>
+        </CRow>
+      </>
     )
   }
 
@@ -92,7 +150,10 @@ const UpdatePost = () => {
               <CCardHeader>
                 <strong>{'Update Post'} </strong>
               </CCardHeader>
-              <CCardBody>{postDetail && renderForm(tableField, postDetail)}</CCardBody>
+              <CCardBody>
+                {state && renderForm(tableField, state)}
+                {renderFormButtons()}
+              </CCardBody>
             </CCard>
           </CCol>
         </CRow>
