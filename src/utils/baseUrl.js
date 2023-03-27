be2a499fe8d530676befb3d71f5f2e16ea6b1cdf
Dewi Sekarini <dewi.sekarini@btpn.com>
2023-03-27 21:59:33 +0700

    fix base url

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,7 +1,7 @@
 import configData from '../appconfig.json'
 
-const baseUrl = 'http://localhost:5002'
-// const baseUrl = configData.MW_SERVER_URL
+// const baseUrl = 'http://localhost:5002'
+const baseUrl = configData.MW_SERVER_URL
 const pythonUrl = configData.PYTHON_SERVER_URL
 
 export { baseUrl, pythonUrl }
